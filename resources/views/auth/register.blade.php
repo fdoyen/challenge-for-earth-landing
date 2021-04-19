54b12c8d206bc46f8d121697234b7f1056d8f065
Florian Doyen <hello@floriandoyen.fr>
2021-04-19 18:22:25 +0200

    add font

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,15 +1,18 @@
 <html>
   <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Challenge For Earth</title>
-    <link href='https://fonts.googleapis.com/css?family=Lato:400,700' rel='stylesheet' type='text/css'>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
     <link rel="icon" type="image/png" href="{{ URL::asset('/images/logo.png') }}" />
     <style>
 
 body {
   background: #F8A434;
-  font-family: 'Lato', sans-serif;
+  font-family: 'Nunito', sans-serif;
+  font-weight: bold;
   color: #FDFCFB;
   text-align: center;
 }
@@ -22,9 +25,10 @@ form {
 
 
 .header {
-  font-size: 25px;
+  font-size: 20px;
   text-transform: uppercase;
   letter-spacing: 5px;
+  font-weight: bold;
 }
 
 
