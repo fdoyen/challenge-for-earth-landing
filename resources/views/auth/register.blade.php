03a01b9497e86ff37dfe3c2b170d39ac4e5ba9e3
Florian Doyen <hello@floriandoyen.fr>
2021-06-07 10:18:23 +0200

    :ambulance: redirect to / and not /register with view

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -139,7 +139,7 @@ input:focus {
               <div class="description">
                 <p>En vous inscrivant, vous concédez à recevoir des nouvelles par email concernant Challenge For Earth.</p>
                 <p>
-                  Session::get('msg');
+                  {{ Session::get('msg'); }}
                 </p>
               </div>
             </form>
